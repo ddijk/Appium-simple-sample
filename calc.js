f89d14e0ad08aab99a242fe95821546b76245fd3
ddijk <dick@dijkrosoft.nl>
2021-04-12 15:25:58 +0200

    Test simple web app

---
+++ calc.js
@@ -0,0 +1,31 @@
+const wdio = require("webdriverio");
+const assert = require("assert");
+
+const opts = {
+  path: '/wd/hub',
+  port: 4723,
+  capabilities: 
+  {
+    "platformName": "Mac",
+    "automationName": "safari"
+  }
+};
+
+async function main () {
+  const client = await wdio.remote(opts);
+
+  await client.navigateTo('http://localhost:8080');
+  const field = await client.$("#x");
+  await field.setValue("4");
+  const field2 = await client.$("#y");
+  await field2.setValue("5");
+  const knop = await client.$("#knop");
+  await knop.click();
+  const resultField = await client.$("#result");
+  const result = await resultField.getText();
+  assert.strictEqual(result,"9");
+
+  await client.deleteSession();
+}
+
+main();
